b3ee2c32a01d562b8553b36d5906984532d7247b
Олеженька <oleganiskov45@gmail.com>
2025-11-27 22:49:04 +0300

    task5

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import numpy as np
-arr = np.arange(0, 21)
-print(arr[:5])
-print(arr[-5:])
-print(arr[::2])
-print(arr[::-1])
+arr = np.arange(25).reshape(5, 5)
+print(arr)
+print(arr[:,1])
+print(arr[3:])
+print(arr[:2, 3:])
+print(arr[4][4])
