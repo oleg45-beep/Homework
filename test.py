31a8e663cb2fb53658d6ca339bc8cc3694178f2c
Олеженька <oleganiskov45@gmail.com>
2025-11-27 23:05:24 +0300

    task8

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import numpy as np
-data = np.random.randint(1, 100, 20)
-print(data.sum())
-print(data.sum()/len(data))
-print(min(data), max(data))
-print(np.argmax(data))
+nums = np.array([12, 45, 2, 78, 31, 5, 96, 14, 67, 23])
+L = nums[nums>30]
+print(L)
+print(nums%2==0)
+M = nums.copy()
+M[M<10]=-1
+print(M)
