591d9b31c07dcfc00f4737a45875bb493af4f76d
Олеженька <oleganiskov45@gmail.com>
2025-11-27 23:36:10 +0300

    task10

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import numpy as np
-linear = np.arange(1, 13)
-print(linear.reshape(3, 4))
-print(linear.reshape(2,3,2))
-print(linear.reshape(3,4).ravel())
+scores = np.array([75, 42, 98, 55, 67, 89, 91, 78, 63, 84, 51, 72, 95, 81, 36, 47, 100, 69, 88, 57])
+print(scores.mean())
+print(min(scores), max(scores))
+print(len(scores[scores>=85]))
+print(len(scores[scores<60]))
+print(np.minimum(scores+5, 100))
