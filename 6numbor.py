99282ff88e21cab5d9cb3df62d0a9009ed80d3a9
Олеженька <oleganiskov45@gmail.com>
2025-12-01 00:35:11 +0300

    Fix6numbor

--- 6numbor.py
+++ 6numbor.py
@@ -1,4 +1,5 @@
 import pandas as pd
 f = pd.read_csv("titanic.csv")
 f['FamilySize'] = f['SibSp'] + f['Parch'] + 1
-print(f.loc[887, 'FamilySize'])
+print(f.loc[888, 'FamilySize'])
+#В первой программе указал по ошибке 887 вместо 888
